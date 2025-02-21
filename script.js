f59407fb9a39d9c9bf175f97bea17451c40074f6
Owen Knudsen <owen.knudsen11@gmail.com>
2025-02-20 20:21:41 -0800

    updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function turnBlue() {
-    console.log("Button clicked!"); // Debug line
+    console.log("Button clicked!");
     document.body.style.backgroundColor = "#0000FF";
     setTimeout(function() {
         document.body.style.backgroundColor = "#f4f4f4";
